c0454a3ac036a300c837a12fade0e9be4f65730b
lwabish <wubw@pku.edu.cn>
2021-12-23 10:43:41 +0800

    feat: NormalizationMem function

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -10,6 +10,10 @@ func Sigmoid(x float64) float64 {
 	return 1 / (1 + math.Exp(-x))
 }
 
+func NormalizationMem(maxBytes int64, valueBytes int64) float64 {
+	return float64(valueBytes) / float64(maxBytes)
+}
+
 const nodeMemoryJsonPath = "data.result.[0].value.[1]"
 
 func ParseNodeMemory(responseString string) (int64, error) {
