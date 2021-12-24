c603ec608fdcf104aecff69791f96c708643943f
lwabish <wubw@pku.edu.cn>
2021-12-24 15:38:53 +0800

    feat: add client-go config

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,7 +2,10 @@ package utils
 
 import (
 	"github.com/thedevsaddam/gojsonq/v2"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	"math"
+	"os"
 	"strconv"
 )
 
@@ -19,4 +22,16 @@ const nodeMemoryJsonPath = "data.result.[0].value.[1]"
 func ParseNodeMemory(responseString string) (int64, error) {
 	r := gojsonq.New().FromString(responseString).Find(nodeMemoryJsonPath)
 	return strconv.ParseInt(r.(string), 10, 64)
+}
+
+// GetClientConfig 获取client-go config,同时兼容集群内和集群外
+func GetClientConfig() (*rest.Config, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		config, err = clientcmd.BuildConfigFromFlags("", os.Getenv("HOME")+"/.kube/config")
+		if err != nil {
+			return nil, err
+		}
+	}
+	return config, nil
 }
