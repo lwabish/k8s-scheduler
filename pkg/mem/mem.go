acda9b0ba5cdd006e3cd6650cff389a11445905c
lwabish <wubw@pku.edu.cn>
2021-12-24 15:39:46 +0800

    feat: add support for fetch node mem usage directly from k8s api server

--- pkg/mem/mem.go
+++ pkg/mem/mem.go
@@ -7,10 +7,12 @@ import (
 	"io"
 	"io/ioutil"
 	v1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
 	runtime2 "k8s.io/kubernetes/pkg/scheduler/framework/runtime"
+	metricsClientSet "k8s.io/metrics/pkg/client/clientset/versioned"
 	"math"
 	"net/http"
 )
@@ -20,6 +22,7 @@ const Name = "NodeAvailableMemory"
 type NodeAvailableMemoryPluginArg struct {
 	PrometheusEndpoint string `json:"prometheus_endpoint,omitempty"`
 	MaxMemory          int    `json:"max_memory,omitempty"`
+	MetricsClientSet   *metricsClientSet.Clientset
 }
 
 type NodeAvailableMemoryPlugin struct {
@@ -31,25 +34,24 @@ func (n NodeAvailableMemoryPlugin) Name() string {
 	return Name
 }
 
-func (n NodeAvailableMemoryPlugin) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
-	queryString := fmt.Sprintf("node_memory_MemAvailable_bytes{kubernetes_node=\"%s\"}", nodeName)
-	r, err := http.Get(fmt.Sprintf("http://%s/api/v1/query?query=%s", n.args.PrometheusEndpoint, queryString))
-	if err != nil {
-		return 0, framework.AsStatus(err)
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
+func (n NodeAvailableMemoryPlugin) Score(_ context.Context, _ *framework.CycleState, _ *v1.Pod, nodeName string) (int64, *framework.Status) {
+	var nodeMemory int64
+	if n.args.PrometheusEndpoint == "" {
+		used, err := n.getNodeUsedMemory(nodeName)
 		if err != nil {
-			panic(err)
+			return 0, framework.AsStatus(err)
 		}
-	}(r.Body)
-	jsonString, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return 0, framework.AsStatus(err)
-	}
-	nodeMemory, err := utils.ParseNodeMemory(string(jsonString))
-	if err != nil {
-		return 0, framework.AsStatus(err)
+		nodeInfo, err := n.handle.SnapshotSharedLister().NodeInfos().Get(nodeName)
+		if err != nil {
+			return 0, framework.AsStatus(err)
+		}
+		nodeMemory = nodeInfo.Allocatable.Memory - used
+	} else {
+		nm, err := n.getNodeAvailableMemory(nodeName)
+		nodeMemory = nm
+		if err != nil {
+			return 0, framework.AsStatus(err)
+		}
 	}
 	normalized := utils.NormalizationMem(int64(n.args.MaxMemory*1024*1024*1024), nodeMemory)
 	sigmoid := utils.Sigmoid(normalized)
@@ -61,9 +63,17 @@ func (n NodeAvailableMemoryPlugin) Score(ctx context.Context, state *framework.C
 func New(configuration runtime.Object, f framework.Handle) (framework.Plugin, error) {
 	args := &NodeAvailableMemoryPluginArg{}
 	err := runtime2.DecodeInto(configuration, args)
+
+	config, err := utils.GetClientConfig()
 	if err != nil {
 		return nil, err
 	}
+	mcs, err := metricsClientSet.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	args.MetricsClientSet = mcs
+
 	return &NodeAvailableMemoryPlugin{
 		handle: f,
 		args:   *args,
@@ -72,4 +82,37 @@ func New(configuration runtime.Object, f framework.Handle) (framework.Plugin, er
 
 func (n *NodeAvailableMemoryPlugin) ScoreExtensions() framework.ScoreExtensions {
 	return nil
+}
+
+// getNodeUsedMemory 输入节点名称，输出节点已用内存量
+func (n *NodeAvailableMemoryPlugin) getNodeUsedMemory(nodeName string) (int64, error) {
+	nodeMetrics, err := n.args.MetricsClientSet.MetricsV1beta1().NodeMetricses().Get(context.TODO(), nodeName, metaV1.GetOptions{})
+	if err != nil {
+		return 0, err
+	}
+	return nodeMetrics.Usage.Memory().Value(), nil
+}
+
+// GetNodeAvailableMemory 输入节点名称和普罗米修斯地址，获取该节点的可用内存量
+func (n *NodeAvailableMemoryPlugin) getNodeAvailableMemory(nodeName string) (int64, error) {
+	queryString := fmt.Sprintf("node_memory_MemAvailable_bytes{kubernetes_node=\"%s\"}", nodeName)
+	r, err := http.Get(fmt.Sprintf("http://%s/api/v1/query?query=%s", n.args.PrometheusEndpoint, queryString))
+	if err != nil {
+		return 0, err
+	}
+	defer func(Body io.ReadCloser) {
+		err = Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(r.Body)
+	jsonString, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return 0, err
+	}
+	nodeMemory, err := utils.ParseNodeMemory(string(jsonString))
+	if err != nil {
+		return 0, err
+	}
+	return nodeMemory, nil
 }
